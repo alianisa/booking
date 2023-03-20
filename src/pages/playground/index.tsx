00a2a5524abab00f359d4de5a17fce99112a7a3e
beaverd2 <beaverrd2@gmail.com>
2023-03-20 03:19:15 +0300

    feat: add heroicons

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -7,6 +7,7 @@ import { Switch } from 'shared/ui/switch'
 import { Textarea } from 'shared/ui/textarea'
 import { ToggleGroup } from 'shared/ui/toggle-group'
 import { ReactNode, useState } from 'react'
+import { Cog6ToothIcon } from '@heroicons/react/24/outline'
 
 export default function Playground() {
   const [toggleGroupValue, setToggleGroupValue] = useState('data2')
@@ -43,38 +44,38 @@ export default function Playground() {
           </Button>
         </div>
         <div className="flex items-center gap-4">
-          <Button leftIcon={<SettingsIcon />} variant="primary" size="sm" disabled>
+          <Button leftIcon={<Cog6ToothIcon className="h-6 w-6" />} variant="primary" size="sm" disabled>
             Disabled
           </Button>
-          <Button leftIcon={<SettingsIcon />} size="sm">
+          <Button leftIcon={<Cog6ToothIcon className="h-6 w-6" />} size="sm">
             Primary
           </Button>
-          <Button leftIcon={<SettingsIcon />} size="md">
+          <Button leftIcon={<Cog6ToothIcon className="h-6 w-6" />} size="md">
             Primary
           </Button>
-          <Button leftIcon={<SettingsIcon />} size="lg">
+          <Button leftIcon={<Cog6ToothIcon className="h-6 w-6" />} size="lg">
             Primary
           </Button>
         </div>
         <div className="flex items-center gap-4">
-          <Button rightIcon={<SettingsIcon />} variant="secondary" size="sm" disabled>
+          <Button rightIcon={<Cog6ToothIcon className="h-6 w-6" />} variant="secondary" size="sm" disabled>
             Disabled
           </Button>
-          <Button variant="secondary" rightIcon={<SettingsIcon />} size="sm">
+          <Button variant="secondary" rightIcon={<Cog6ToothIcon className="h-6 w-6" />} size="sm">
             Primary
           </Button>
-          <Button variant="secondary" rightIcon={<SettingsIcon />} size="md">
+          <Button variant="secondary" rightIcon={<Cog6ToothIcon className="h-6 w-6" />} size="md">
             Primary
           </Button>
-          <Button variant="secondary" rightIcon={<SettingsIcon />} size="lg">
+          <Button variant="secondary" rightIcon={<Cog6ToothIcon className="h-6 w-6" />} size="lg">
             Primary
           </Button>
         </div>
         <div className="flex items-center gap-4">
-          <IconButton icon={<SettingsIcon />} variant="secondary" size="sm" disabled />
-          <IconButton icon={<SettingsIcon />} variant="secondary" size="sm" />
-          <IconButton icon={<SettingsIcon />} variant="secondary" size="md" />
-          <IconButton icon={<SettingsIcon />} variant="secondary" size="lg" />
+          <IconButton icon={<Cog6ToothIcon className="h-6 w-6" />} variant="secondary" size="sm" disabled />
+          <IconButton icon={<Cog6ToothIcon className="h-6 w-6" />} variant="secondary" size="sm" />
+          <IconButton icon={<Cog6ToothIcon className="h-6 w-6" />} variant="secondary" size="md" />
+          <IconButton icon={<Cog6ToothIcon className="h-6 w-6" />} variant="secondary" size="lg" />
         </div>
         <div className="flex items-center gap-4">
           <ToggleGroup
@@ -97,8 +98,8 @@ export default function Playground() {
           <ToggleGroup
             value={toggleGroupValue3}
             data={[
-              { label: <SettingsIcon />, value: 'variant1' },
-              { label: <SettingsIcon />, value: 'variant2' },
+              { label: <Cog6ToothIcon className="h-6 w-6" />, value: 'variant1' },
+              { label: <Cog6ToothIcon className="h-6 w-6" />, value: 'variant2' },
             ]}
             onChange={(value) => setToggleGroupValue3(value)}
           />
@@ -106,8 +107,8 @@ export default function Playground() {
             variant="icon"
             value={toggleGroupValue3}
             data={[
-              { label: <SettingsIcon />, value: 'variant1' },
-              { label: <SettingsIcon />, value: 'variant2' },
+              { label: <Cog6ToothIcon className="h-6 w-6" />, value: 'variant1' },
+              { label: <Cog6ToothIcon className="h-6 w-6" />, value: 'variant2' },
             ]}
             onChange={(value) => setToggleGroupValue3(value)}
           />
@@ -174,7 +175,7 @@ export default function Playground() {
             placeholder="leftElement"
             error="This is an error message."
             variant="filled"
-            leftElement={<SettingsIcon />}
+            leftElement={<Cog6ToothIcon className="h-6 w-6" />}
           />
           <Input
             name="search2"
@@ -182,7 +183,7 @@ export default function Playground() {
             placeholder="rightElement"
             hint="This is a hint text to help user."
             variant="filled"
-            rightElement={<SettingsIcon />}
+            rightElement={<Cog6ToothIcon className="h-6 w-6" />}
           />
           <Input
             name="search3"
@@ -322,26 +323,5 @@ const Block = ({ title, children }: { title?: string; children: ReactNode }) => 
       <h2 className="text-lg font-bold">{title}</h2>
       {children}
     </div>
-  )
-}
-
-const SettingsIcon = (props: any) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-6 w-6"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-      />
-      <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-    </svg>
   )
 }
