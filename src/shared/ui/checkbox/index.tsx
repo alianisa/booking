3d601c62eec84191132259aaf84286f39d6815f3
beaverd2 <beaverrd2@gmail.com>
2023-03-20 22:47:19 +0300

    refactor: refactor checkbox styles

--- src/shared/ui/checkbox/index.tsx
+++ src/shared/ui/checkbox/index.tsx
@@ -10,18 +10,17 @@ type Props = {
 
 export const Checkbox = ({ name, label, size = 'md', className, ...props }: Props) => {
   const inputStyles = clsx(
-    'rounded border-gray-300 text-gray-800 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-60 transition-colors duration-100 cursor-pointer',
+    'peer ounded border-gray-300 text-gray-800 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-60 transition-colors duration-100 cursor-pointer',
     size === 'sm' && 'h-4 w-4',
     size === 'md' && 'h-5 w-5'
   )
   const labelStyles = clsx(
-    'text-md ml-3 select-none font-medium text-gray-700 cursor-pointer',
+    'text-md ml-3 select-none font-medium text-gray-700 cursor-pointer peer-disabled:opacity-60 peer-disabled:cursor-not-allowed',
     size === 'sm' && 'text-sm',
-    size === 'md' && 'text-base',
-    props?.disabled && '!cursor-not-allowed opacity-60'
+    size === 'md' && 'text-base'
   )
   return (
-    <div className={cn('flex items-center', props?.disabled && 'cursor-not-allowed', className)}>
+    <div className={cn('flex items-center', className)}>
       <input {...props} name={name} id={name} type="checkbox" className={inputStyles} />
       {label && (
         <label htmlFor={name} className={labelStyles}>
