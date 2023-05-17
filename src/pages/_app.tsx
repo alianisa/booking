55dee13e9b110914f95026e9751c3509a7ebfc98
beaverd2 <beaverrd2@gmail.com>
2023-05-17 04:15:32 +0300

    refactor: remove react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app'
 import { YMaps } from '@pbe/react-yandex-maps'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -9,15 +7,11 @@ import { Layout } from 'widgets'
 import 'shared/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
   return (
-    <QueryClientProvider client={queryClient}>
-      <YMaps>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </YMaps>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    <YMaps>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </YMaps>
   )
 }
